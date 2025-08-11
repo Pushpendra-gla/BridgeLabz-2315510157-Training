8394a51d7faeaf8ab1db46bc2d27e52ffed140ca
Pushpendra-gla <pushpendra.gla_cs.aiml23@gla.ac.in>
2025-08-11 10:32:04 +0530

    edited this file

--- Practice_Problems/Add_2Numbers.java
+++ Practice_Problems/Add_2Numbers.java
@@ -1,10 +1,14 @@
 package Practice_Problems;
-
+import java.util.Scanner;
 public class Add_2Numbers {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
         int sum = num1 + num2;
+        scanner.close();
         System.out.println("Sum: " + sum);
     }
 } 
