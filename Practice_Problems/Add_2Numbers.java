db346f015b275d6b2dd89a49608f6583c933ff4a
Pushpendra-gla <pushpendra.gla_cs.aiml23@gla.ac.in>
2025-08-11 10:27:54 +0530

    Added two_numbers file

---
+++ Practice_Problems/Add_2Numbers.java
@@ -0,0 +1,10 @@
+package Practice_Problems;
+
+public class Add_2Numbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+} 
